9c5f4c2e5d5b920b0817f48ce827b3749e785891
Joc2108 <josetorresaguilar21@gmail.com>
2023-11-17 16:47:21 -0500

    llenando dashboard

---
+++ app/lib/datosnew.ts
@@ -0,0 +1,52 @@
+import {
+    CustomerField,
+    CustomersTable,
+    InvoiceForm,
+    InvoicesTable,
+    LatestInvoiceRaw,
+    User,
+    Revenue,
+  } from './definitions';
+  import { formatCurrency } from './utils';
+  import Datos from "@/app/lib/placeholder-data"
+
+  export async function fetchRevenue() {
+       try {
+           const data = await Datos.revenue;
+              return data;
+    } catch (error) {
+      console.error('Error cargando datos', error);
+      throw new Error('Failed to fetch revenue data.');
+    }
+  }
+
+  export async function fetchLatestInvoices() {
+    try {
+      let data =  Datos.invoices
+      data.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+      });
+      var Datatemp: Array<LatestInvoiceRaw> = []
+      await data.map((datos,index) => {
+        let cliente = Datos.customers.find(clienteTemp =>  clienteTemp.id=== datos.customer_id);
+console.log(cliente.id)
+        Datatemp.push({"id": cliente.id,
+        "name": cliente.name,
+        "image_url": cliente?.image_url,
+        "email": cliente?.email,
+        "amount": datos.amount})
+      })
+      
+      
+      Datatemp = Datatemp.slice(0,5)
+      
+      const latestInvoices = Datatemp.map((invoice) => ({
+        ...invoice,
+        amount: formatCurrency(invoice.amount),
+      }));
+      return latestInvoices;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch the latest invoices.');
+    }
+  }
